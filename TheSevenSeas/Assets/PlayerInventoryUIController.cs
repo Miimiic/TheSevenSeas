50bc3ef0c72f458fd44dd915c3eb0735f219ef47
SamW1401 <25879271@stu.mmu.ac.uk>
2026-02-27 01:10:37 +0000

    Added Inventory
    Added inventory system
    Singleton, so easy for other scripts to add resources via functions
    Inventory UI, opens with Tab and Updates to reflect current resources
    Armour system within the health controller, Reduces damage by ( Armour Level * 30 ) every time the player is damaged. This can be changed if its too much reduction for balancing purposes

---
+++ TheSevenSeas/Assets/PlayerInventoryUIController.cs
@@ -0,0 +1,58 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerInventoryUIController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Objects")]
+    [SerializeField] GameObject parentUiObject;
+
+    [Header("Text Components")]
+    [SerializeField] private TMP_Text basicMaterialText;
+    [SerializeField] private TMP_Text rareMaterialText;
+    [SerializeField] private TMP_Text healthText;
+    [SerializeField] private TMP_Text armourText;
+
+    // The in-game health text, Disabled when inventory opens
+    [SerializeField] private TMP_Text mainHealthText;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleInventoryUI(); 
+        }
+    }
+
+    private void HandleInventoryUI()
+    {
+        basicMaterialText.text = " Wood : " + PlayerInventoryController.Instance.GetWoodAmount() + "\n Brick : " + PlayerInventoryController.Instance.GetBrickAmount() + "\n Metal : " + PlayerInventoryController.Instance.GetMetalAmount();
+
+        rareMaterialText.text = " Nails : " + PlayerInventoryController.Instance.GetNailAmount() + "\n Pipes : " + PlayerInventoryController.Instance.GetMetalPipeAmount() + 
+            "\n Manuals : " + PlayerInventoryController.Instance.GetInstructionManualAmount() + "\n Flesh : " + PlayerInventoryController.Instance.GetWorkerFleshAmount() +
+            "\n Hearts : " + PlayerInventoryController.Instance.GetHeldritchHeartAmount();
+
+        healthText.text = " Health : " + PlayerHealthController.Instance.GetCurrentHealth() + " / " + PlayerHealthController.Instance.GetMaxHealth();
+
+        armourText.text = " Armour Level : " + PlayerHealthController.Instance.GetArmourLevel() + "\n Armour Damage Reduction - " + PlayerHealthController.Instance.GetArmourReduction();
+    }
+
+    public void ToggleInventoryUI()
+    {
+        if (parentUiObject.gameObject.activeInHierarchy)
+        {
+            HandleInventoryUI();
+            mainHealthText.gameObject.SetActive(true);
+            parentUiObject.gameObject.SetActive(false);
+        }
+        else
+        {
+            HandleInventoryUI();
+            mainHealthText.gameObject.SetActive(false);
+            parentUiObject.gameObject.SetActive(true);  
+        }
+    }
+}
