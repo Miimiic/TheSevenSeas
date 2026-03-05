c748d1e16536d2becd5e89dc5a8181e49e456f68
Daniel-Newton902 <daniel2008newton@gmail.com>
2026-03-05 11:42:22 +0000

    Each gun button has the stats

--- TheSevenSeas/Assets/Scripts/UI/CraftingSystem.cs
+++ TheSevenSeas/Assets/Scripts/UI/CraftingSystem.cs
@@ -7,28 +7,36 @@ public class CraftingSystem : MonoBehaviour
 {
 
     //Specific for each button
-    public int buttonNo;
     public List<TextMeshProUGUI> textList;
-    public List<string> nameList = new List<string>();
-    public List<string> damageList = new List<string>();
-    public List<string> speedList = new List<string>();
+    public string name;
+    public int damage;
+    public int speed;
+    public int wood;
+    public int brick;
+    public int metal;
+    public int nails;
+    public int pipes;
+    public int manuals;
+
 
 
     void Start()
     {
-        textList = new List<TextMeshProUGUI>();
-
-        nameList = new List<string>();
-        damageList = new List<string>();
-        speedList = new List<string>();
 
     }
 
     public void ButtonClick () {
         //Update the text
-        textList[0].text = "Name: "+nameList[buttonNo];
-        textList[1].text = "Damage: "+damageList[buttonNo];
-        textList[2].text = "Speed: "+speedList[buttonNo];
+        textList[0].text = "Name: "+name;
+        textList[1].text = "Damage: "+damage;
+        textList[2].text = "Speed: "+speed;
+        textList[3].text = "-x " + wood;
+        textList[4].text = "-x " + brick;
+        textList[5].text = "-x " + metal;
+        textList[6].text = "-x " + nails;
+        textList[7].text = "-x " + pipes;
+        textList[8].text = "-x " + manuals;
+
 
     }
 }
